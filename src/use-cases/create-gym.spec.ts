7c540790654d9548ea12caf12b028aac82b474d6
Bruno Fukumori <brunofukumori@gmail.com>
2023-10-04 23:42:44 -0300

    feat: list check ins history by user and pagination

--- src/use-cases/create-gym.spec.ts
+++ src/use-cases/create-gym.spec.ts
@@ -4,28 +4,19 @@ import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-
 
 let inMemoryGymsRepository: InMemoryGymsRepository;
 let sut: CreateGymUseCase;
-let requestBody: {
-  title: string;
-  description?: string;
-  phone?: string;
-  latitude: number;
-  longitude: number;
-};
 
 describe('Create gym use case', () => {
   beforeEach(() => {
     inMemoryGymsRepository = new InMemoryGymsRepository();
     sut = new CreateGymUseCase(inMemoryGymsRepository);
-
-    requestBody = {
-      title: 'Gym Ignite',
-      latitude: -23.5153507,
-      longitude: -46.3468447,
-    };
   });
 
   it('should be able to register', async () => {
-    const { gym } = await sut.execute(requestBody);
+    const { gym } = await sut.execute({
+      title: 'Gym Ignite',
+      latitude: -23.5153507,
+      longitude: -46.3468447,
+    });
 
     expect(gym.id).toEqual(expect.any(String));
   });
