7c35d1036aedc6222606ad2354085caa0f3f3147
Bruno Fukumori <brunofukumori@gmail.com>
2023-10-08 16:47:18 -0300

    feat: check-ins controllers

---
+++ src/http/controllers/check-ins/create.ts
@@ -0,0 +1,31 @@
+import { makeCheckInsUseCase } from '@/use-cases/factories/make-check-in-use-case';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const createCheckInParamsSchema = z.object({
+    gymId: z.string().uuid(),
+  });
+  const createCheckInBodySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90;
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180;
+    }),
+  });
+
+  const { latitude, longitude } = createCheckInBodySchema.parse(request.body);
+  const { gymId } = createCheckInParamsSchema.parse(request.params);
+
+  const checkInsUseCase = makeCheckInsUseCase();
+
+  await checkInsUseCase.execute({
+    gymId,
+    userLatitude: latitude,
+    userLongitude: longitude,
+    userId: request.user.sub,
+  });
+
+  return reply.status(201).send();
+}
